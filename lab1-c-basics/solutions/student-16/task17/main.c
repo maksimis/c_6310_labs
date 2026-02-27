edfd81bd9b4588559ae1b286b916664d91025f60
Родион Карпов <r.karpov3332mail.com@mail.ru>
2026-02-27 16:42:25 +0300

    some fix in struct of main.c

--- lab1-c-basics/solutions/student-16/task17/main.c
+++ lab1-c-basics/solutions/student-16/task17/main.c
@@ -1,24 +1,13 @@
 /*
  * Лабораторная работа №1
- * Студент: 16
+ * Студент: Карпов Родион (номер: 16)
  * Вариант: 17
  * Задание: 17 (Наименьшее общее кратное двух чисел)
  */
 
 #include <stdio.h>
-
-static int gcd(int first, int second) {
-    while (second != 0) {
-        int temp = second;
-        second = first % second;
-        first = temp;
-    }
-    return first;
-}
-
-static int lcm(int first, int second) {
-    return first / gcd(first, second) * second;
-}
+static int gcd(int first, int second);
+static int lcm(int first, int second);
 
 int main(void) {
     int first, second;
@@ -37,4 +26,17 @@ int main(void) {
     printf("НОК(%d, %d) = %d\n", first, second, lcm(first, second));
 
     return 0;
+}
+
+static int gcd(int first, int second) {
+    while (second != 0) {
+        int temp = second;
+        second = first % second;
+        first = temp;
+    }
+    return first;
+}
+
+static int lcm(int first, int second) {
+    return first / gcd(first, second) * second;
 }
