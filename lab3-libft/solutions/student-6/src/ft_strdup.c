6a55bb2e8063649ce0959ae160da496ad41c479d
Марсель Валиев <mitro2021@gmail.com>
2026-03-27 17:18:18 +0300

    Lab3 solution by student6

---
+++ lab3-libft/solutions/student-6/src/ft_strdup.c
@@ -0,0 +1,14 @@
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	char	*dup;
+	size_t	len;
+
+	len = ft_strlen(s) + 1;
+	dup = malloc(len);
+	if (!dup)
+		return (NULL);
+	ft_memcpy(dup, s, len);
+	return (dup);
+}
