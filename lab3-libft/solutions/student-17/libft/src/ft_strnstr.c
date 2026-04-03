1f4fb03b46872bcf79b146e78cac94286c84735e
albertkayumov <albertjj050@gmail.com>
2026-04-03 23:05:19 +0300

    3lab

---
+++ lab3-libft/solutions/student-17/libft/src/ft_strnstr.c
@@ -0,0 +1,20 @@
+#include "libft.h"
+
+char *ft_strnstr(const char *big, const char *little, size_t len) {
+    size_t little_len;
+    size_t i;
+    
+    if (!*little)
+        return ((char *)big);
+    
+    little_len = ft_strlen(little);
+    i = 0;
+    
+    while (big[i] && i + little_len <= len)
+    {
+        if (ft_strncmp(&big[i], little, little_len) == 0)  
+            return ((char *)&big[i]);
+        i++;
+    }
+    return (NULL);
+}
