afd70acc4eab8d68ec8604ba5a54bd25a5e4af8a
Asker-Crafter <autumn-26@yandex.ru>
2026-04-25 20:25:14 +0300

    completed task

---
+++ lab5/solutions/student-20/src/eq_matrix.c
@@ -0,0 +1,25 @@
+#include "matrix.h"
+
+int	eq_matrix(matrix_t *A, matrix_t *B)
+{
+	int	i;
+	int	j;
+
+	if (!A || !B)
+		return (0);
+	if (A->rows != B->rows || A->columns != B->columns)
+		return (0);
+	i = 0;
+	while (i < A->rows)
+	{
+		j = 0;
+		while (j < A->columns)
+		{
+			if (A->matrix[i][j] != B->matrix[i][j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
