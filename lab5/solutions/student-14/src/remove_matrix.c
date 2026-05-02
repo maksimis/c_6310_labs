b1686bb74a53a3dba5af4f664318dd2a35993686
Дина <ziatdinovadina73@gmail.com>
2026-05-02 21:52:48 +0300

    Lab 5 solution by student 14

---
+++ lab5/solutions/student-14/src/remove_matrix.c
@@ -0,0 +1,22 @@
+#include "matrix.h"
+
+void remove_matrix(matrix_t *A)
+{
+	int	i;
+
+	if (!A)
+		return ;
+		
+	if (A->matrix)
+	{
+		i = 0;
+		while (i < A->rows)
+		{
+			if (A->matrix[i])
+				free(A->matrix[i]);
+			i++;
+		}
+		free(A->matrix);
+	}
+	free(A);
+}
