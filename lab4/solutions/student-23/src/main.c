e55bcfc800e02e9815f0e69fe9662162441ced13
aiamil <isadyrttinova@bk.ru>
2026-05-12 20:36:06 +0300

    Lab 4 solution | Student 23  | Variant 3

--- lab4/solutions/student-23/src/main.c
+++ lab4/solutions/student-23/src/main.c
@@ -1,249 +1,199 @@
 #include "libft.h"
 #include <stdarg.h>
 
-// Коды метрик
-#define METRIC_MIN 0
-#define METRIC_MAX 1
-#define METRIC_SUM 2
-#define METRIC_AVG 3
+typedef int	(*t_func)(int *, int);
 
-// Прототипы функций статистики
-int min_impl(int count, ...);
-int max_impl(int count, ...);
-int sum_impl(int count, ...);
-int avg_impl(int count, ...);
-
-// Таблица переходов (массив указателей на вариативные функции)
-int (*g_metrics[])(int, ...) = {
-    min_impl,
-    max_impl,
-    sum_impl,
-    avg_impl
-};
-
-// Реализация MIN (вариативная функция)
-int min_impl(int count, ...)
+typedef struct s_op
 {
-    va_list args;
-    int min_val;
-    int current;
-    int i;
+	char	*name;
+	t_func	func;
+	int		metric;
+}	t_op;
 
-    if (count <= 0)
-        return (0);
-    va_start(args, count);
-    min_val = va_arg(args, int);
-    i = 1;
-    while (i < count)
-    {
-        current = va_arg(args, int);
-        if (current < min_val)
-            min_val = current;
-        i++;
-    }
-    va_end(args);
-    return (min_val);
+/*
+** Variadic function
+*/
+int	compute(int metric, int count, ...)
+{
+	va_list	args;
+	int		i;
+	int		val;
+	int		res;
+
+	if (count <= 0)
+		return (0);
+	va_start(args, count);
+	res = va_arg(args, int);
+	i = 1;
+	while (i < count)
+	{
+		val = va_arg(args, int);
+		if (metric == 0 && val < res)
+			res = val;
+		else if (metric == 1 && val > res)
+			res = val;
+		else if (metric == 2 || metric == 3)
+			res += val;
+		i++;
+	}
+	va_end(args);
+	if (metric == 3)
+		return (res / count);
+	return (res);
 }
 
-// Реализация MAX
-int max_impl(int count, ...)
+int	ft_min_arr(int *arr, int count)
 {
-    va_list args;
-    int max_val;
-    int current;
-    int i;
+	int	i;
+	int	min;
 
-    if (count <= 0)
-        return (0);
-    va_start(args, count);
-    max_val = va_arg(args, int);
-    i = 1;
-    while (i < count)
-    {
-        current = va_arg(args, int);
-        if (current > max_val)
-            max_val = current;
-        i++;
-    }
-    va_end(args);
-    return (max_val);
+	min = arr[0];
+	i = 1;
+	while (i < count)
+	{
+		if (arr[i] < min)
+			min = arr[i];
+		i++;
+	}
+	return (min);
 }
 
-// Реализация SUM
-int sum_impl(int count, ...)
+int	ft_max_arr(int *arr, int count)
 {
-    va_list args;
-    int total;
-    int i;
+	int	i;
+	int	max;
 
-    total = 0;
-    va_start(args, count);
-    i = 0;
-    while (i < count)
-    {
-        total += va_arg(args, int);
-        i++;
-    }
-    va_end(args);
-    return (total);
+	max = arr[0];
+	i = 1;
+	while (i < count)
+	{
+		if (arr[i] > max)
+			max = arr[i];
+		i++;
+	}
+	return (max);
 }
 
-// Реализация AVG (округление вниз)
-int avg_impl(int count, ...)
+int	ft_sum_arr(int *arr, int count)
 {
-    va_list args;
-    int total;
-    int i;
+	int	i;
+	int	sum;
 
-    total = 0;
-    va_start(args, count);
-    i = 0;
-    while (i < count)
-    {
-        total += va_arg(args, int);
-        i++;
-    }
-    va_end(args);
-    if (count > 0)
-        return (total / count);
-    return (0);
+	sum = 0;
+	i = 0;
+	while (i < count)
+	{
+		sum += arr[i];
+		i++;
+	}
+	return (sum);
 }
 
-// Получение кода метрики по строковому имени
-int get_metric_code(char *name)
+int	ft_avg_arr(int *arr, int count)
 {
-    if (ft_strncmp(name, "min", 3) == 0 && ft_strlen(name) == 3)
-        return (METRIC_MIN);
-    if (ft_strncmp(name, "max", 3) == 0 && ft_strlen(name) == 3)
-        return (METRIC_MAX);
-    if (ft_strncmp(name, "sum", 3) == 0 && ft_strlen(name) == 3)
-        return (METRIC_SUM);
-    if (ft_strncmp(name, "avg", 3) == 0 && ft_strlen(name) == 3)
-        return (METRIC_AVG);
-    return (-1);
-}
-// Проверка, является ли строка числом
-int is_number(char *str)
-{
-    int i;
-
-    i = 0;
-    if (str[i] == '-' || str[i] == '+')
-        i++;
-    if (!str[i])
-        return (0);
-    while (str[i])
-    {
-        if (str[i] < '0' || str[i] > '9')
-            return (0);
-        i++;
-    }
-    return (1);
+	return (ft_sum_arr(arr, count) / count);
 }
 
-int main(int argc, char **argv)
+t_func	get_func(char *name)
 {
-    int metric_code;
-    int *numbers;
-    int count;
-    int result;
-    int i;
+	t_op	ops[5];
+	int		i;
 
-    // Проверка минимального количества аргументов: программа + метрика + хотя бы одно число
-    if (argc < 3)
-    {
-        ft_putstr_fd("Ошибка: недостаточно аргументов.\n", 2);
-        ft_putstr_fd("Формат: ./stats <min|max|sum|avg> [числа...]\n", 2);
-        return (1);
-    }
+	ops[0] = (t_op){"min", ft_min_arr, 0};
+	ops[1] = (t_op){"max", ft_max_arr, 1};
+	ops[2] = (t_op){"sum", ft_sum_arr, 2};
+	ops[3] = (t_op){"avg", ft_avg_arr, 3};
+	ops[4] = (t_op){NULL, NULL, -1};
 
-    // Получение кода метрики
-    metric_code = get_metric_code(argv[1]);
-    if (metric_code == -1)
-    {
-        ft_putstr_fd("Ошибка: неизвестная метрика.\n", 2);
-        ft_putstr_fd("Доступные метрики: min, max, sum, avg\n", 2);
-        return (1);
-    }
+	i = 0;
+	while (ops[i].name)
+	{
+		if (ft_strncmp(name, ops[i].name, 4) == 0)
+			return (ops[i].func);
+		i++;
+	}
+	return (NULL);
+}
 
-    // Количество чисел
-    count = argc - 2;
-    if (count == 0)
-    {
-        ft_putstr_fd("Ошибка: список чисел пуст.\n", 2);
-        return (1);
-    }
+int	get_metric(char *name)
+{
+	t_op	ops[5];
+	int		i;
 
-    // Выделение памяти под массив чисел (можно и без массива, но для проверки корректности)
-    numbers = (int *)malloc(count * sizeof(int));
-    if (!numbers)
-    {
-        ft_putstr_fd("Ошибка: не удалось выделить память.\n", 2);
-        return (1);
-    }
+	ops[0] = (t_op){"min", ft_min_arr, 0};
+	ops[1] = (t_op){"max", ft_max_arr, 1};
+	ops[2] = (t_op){"sum", ft_sum_arr, 2};
+	ops[3] = (t_op){"avg", ft_avg_arr, 3};
+	ops[4] = (t_op){NULL, NULL, -1};
 
-    // Преобразование строк в числа и проверка
-    i = 0;
-    while (i < count)
-    {
-        if (!is_number(argv[i + 2]))
-        {
-            ft_putstr_fd("Ошибка: аргумент '", 2);
-            ft_putstr_fd(argv[i + 2], 2);
-            ft_putstr_fd("' не является целым числом.\n", 2);
-            free(numbers);
-            return (1);
-        }
-        numbers[i] = ft_atoi(argv[i + 2]);
-        i++;
-    }
+	i = 0;
+	while (ops[i].name)
+	{
+		if (ft_strncmp(name, ops[i].name, 4) == 0)
+			return (ops[i].metric);
+		i++;
+	}
+	return (-1);
+}
 
-    // Вызов функции через таблицу переходов с переменным числом аргументов
-    // Передаем count и все числа через ...
-    switch (metric_code)
-    {
-    case METRIC_MIN:
-        result = g_metrics[METRIC_MIN](count, 
-                    numbers[0], (count > 1) ? numbers[1] : 0, 
-                    (count > 2) ? numbers[2] : 0, (count > 3) ? numbers[3] : 0,
-                    (count > 4) ? numbers[4] : 0, (count > 5) ? numbers[5] : 0,
-                    (count > 6) ? numbers[6] : 0, (count > 7) ? numbers[7] : 0,
-                    (count > 8) ? numbers[8] : 0, (count > 9) ? numbers[9] : 0);
-        break;
-    case METRIC_MAX:
-        result = g_metrics[METRIC_MAX](count, 
-                    numbers[0], (count > 1) ? numbers[1] : 0, 
-                    (count > 2) ? numbers[2] : 0, (count > 3) ? numbers[3] : 0,
-                    (count > 4) ? numbers[4] : 0, (count > 5) ? numbers[5] : 0,
-                    (count > 6) ? numbers[6] : 0, (count > 7) ? numbers[7] : 0,
-                    (count > 8) ? numbers[8] : 0, (count > 9) ? numbers[9] : 0);
-        break;
-    case METRIC_SUM:
-        result = g_metrics[METRIC_SUM](count, 
-                    numbers[0], (count > 1) ? numbers[1] : 0, 
-                    (count > 2) ? numbers[2] : 0, (count > 3) ? numbers[3] : 0,
-                    (count > 4) ? numbers[4] : 0, (count > 5) ? numbers[5] : 0,
-                    (count > 6) ? numbers[6] : 0, (count > 7) ? numbers[7] : 0,
-                    (count > 8) ? numbers[8] : 0, (count > 9) ? numbers[9] : 0);
-        break;
-    case METRIC_AVG:
-        result = g_metrics[METRIC_AVG](count, 
-                    numbers[0], (count > 1) ? numbers[1] : 0, 
-                    (count > 2) ? numbers[2] : 0, (count > 3) ? numbers[3] : 0,
-                    (count > 4) ? numbers[4] : 0, (count > 5) ? numbers[5] : 0,
-                    (count > 6) ? numbers[6] : 0, (count > 7) ? numbers[7] : 0,
-                    (count > 8) ? numbers[8] : 0, (count > 9) ? numbers[9] : 0);
-        break;
-    default:
-        result = 0;
-        break;
-    }
+int	main(int argc, char **argv)
+{
+	int		*nums;
+	int		count;
+	int		i;
+	t_func	func;
+	int		result;
+	int		metric;
 
-    // Вывод результата
-    ft_putnbr_fd(result, 1);
-    ft_putchar_fd('\n', 1);
+	if (argc < 3)
+		return (ft_putstr_fd("Error: not enough arguments\n", 2), 1);
 
-    // Очистка памяти
-    free(numbers);
-    return (0);
+	func = get_func(argv[1]);
+	metric = get_metric(argv[1]);
+
+	if (!func || metric == -1)
+		return (ft_putstr_fd("Error: unknown metric\n", 2), 1);
+
+	count = argc - 2;
+
+	nums = (int *)malloc(sizeof(int) * count);
+	if (!nums)
+		return (ft_putstr_fd("Error: malloc failed\n", 2), 1);
+
+	i = 0;
+	while (i < count)
+	{
+		nums[i] = ft_atoi(argv[i + 2]);
+		i++;
+	}
+
+	/*
+	** Использование указателя на функцию
+	*/
+	func(nums, count);
+
+	/*
+	** Использование variadic function
+	*/
+	if (count == 1)
+		result = compute(metric, 1,
+				nums[0]);
+	else if (count == 2)
+		result = compute(metric, 2,
+				nums[0], nums[1]);
+	else if (count == 3)
+		result = compute(metric, 3,
+				nums[0], nums[1], nums[2]);
+	else if (count == 4)
+		result = compute(metric, 4,
+				nums[0], nums[1], nums[2], nums[3]);
+	else
+		result = compute(metric, 5,
+				nums[0], nums[1], nums[2], nums[3], nums[4]);
+
+	ft_putnbr_fd(result, 1);
+	ft_putchar_fd('\n', 1);
+
+	free(nums);
+	return (0);
 }
